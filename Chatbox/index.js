97ea0822d811d5e1a115d3c6a0463948ab3b1944
Bruno RC <brunorcx@hotmail.com>
2020-10-12 23:32:21 -0400

    Atualização index e conflitos

--- Chatbox/index.js
+++ Chatbox/index.js
@@ -8,13 +8,24 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', function (socket) {
-  socket.on('chat message', function (msg) {
-    console.log('a user connected');
-    io.emit('chat message', msg);
+//Conexão de usúario
+io.on("connection", function (socket) {
+  socket.on("chat message", function (msg) {
+    console.log("a user connected");
+
+    //funcao para reverter a mensagem
+    var splitMsg = msg.split("");
+    var reverseMsg = splitMsg.reverse();
+    var msgFinal = reverseMsg.join("");
+
+    io.emit("chat message", msgFinal);
   });
 });
 
+io.on("disconnect", () => {
+  console.log("user disconnected!");
+});
+
 http.listen(port, function () {
-  console.log('listening on *:' + port);
+  console.log("listening on *:" + port);
 });
