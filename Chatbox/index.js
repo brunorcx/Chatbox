01ae82565fb8581491d6619f3a4ab0a356b84fd2
Bruno RC <brunorcx@hotmail.com>
2020-10-12 23:25:24 -0400

    Função DoS e Atualização da função tamanhoMensagens

--- Chatbox/index.js
+++ Chatbox/index.js
@@ -10,11 +10,41 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
   socket.on('chat message', function (msg) {
-    console.log('a user connected');
-    io.emit('chat message', msg);
+    console.log('Um cliente se conectou');
+    if (isNaN(msg))
+      io.emit('chat message', msg);
+    else
+      io.emit('chat message', tamanhoMensagens(msg));
+
   });
 });
 
 http.listen(port, function () {
   console.log('listening on *:' + port);
 });
+
+function tamanhoMensagens(numByte) {
+  var texto = "";
+  for (var i = 0; i < numByte; i++) {
+    texto = texto + "a";
+    if (numByte % 100 == 0)
+      texto = texto + "\n";
+  }
+  return texto;
+}
+
+var sIO = {};
+
+sIO.on = (function () {
+  var messages = {};
+  var speedLimit = 5; //5ms
+  return function (message, handler) {
+    messages[message] = messages[message] || {};
+    if (messages[message].timestamp && new Date().getTime() - messages[message].timestamp < speedLimit) return false;
+    else messages[message].timestamp = new Date().getTime();
+
+    handler();
+    return true;
+    //execute code, Ex:
+  }
+}());
